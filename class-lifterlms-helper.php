709317d394b64711d23e2fd00463ff0f06627795
Brian Hogg <brian@bhconsulting.ca>
2024-07-29 06:47:35 -0400

    Prefixing the composer packages to avoid conflicts.

--- class-lifterlms-helper.php
+++ class-lifterlms-helper.php
@@ -146,7 +146,7 @@ final class LifterLMS_Helper {
 	 */
 	private function includes() {
 
-		require_once 'vendor/autoload.php';
+		require_once 'vendor-prefixed/autoload.php';
 
 		require_once LLMS_HELPER_PLUGIN_DIR . 'includes/class-llms-helper-admin-add-ons.php';
 		require_once LLMS_HELPER_PLUGIN_DIR . 'includes/class-llms-helper-assets.php';
